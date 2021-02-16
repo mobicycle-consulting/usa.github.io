d39cc7180881f7ddfd78b6e235da106bfafb3ec4
Rose Scott <rose@mobicycle.eu>
2021-02-16 01:19:14 +0000

    start from scratch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import Home2 from './components/Home2';
-import Header from './components/Header';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <> <Header />
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
