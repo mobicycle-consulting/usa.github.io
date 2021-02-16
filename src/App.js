b9de24644932ca721b663018ed825468a239e4fb
Rose Scott <rose@mobicycle.eu>
2021-02-16 00:41:44 +0000

    styling

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <Header/>;
-<Body/>;
-<Pricing/>;
-<Footer/>;
-    </div>
+    <>
+      <Header/>
+      <Body/>
+      <Pricing/>
+      <Footer/>
+    </>
   )
 }
 
