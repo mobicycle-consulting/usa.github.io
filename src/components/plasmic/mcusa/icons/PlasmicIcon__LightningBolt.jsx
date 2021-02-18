8f3c8924f7359e41bf83a0b7d3c828cc06119d87
Rose Scott <rose@mobicycle.eu>
2021-02-18 10:20:17 +0000

    add new page

---
+++ src/components/plasmic/mcusa/icons/PlasmicIcon__LightningBolt.jsx
@@ -0,0 +1,38 @@
+// @ts-nocheck
+/* eslint-disable */
+/* tslint:disable */
+/* prettier-ignore-start */
+import React from "react";
+import { classNames } from "@plasmicapp/react-web";
+
+export function LightningBoltIcon(props) {
+  const { className, style, title, ...restProps } = props;
+  return (
+    <svg
+      xmlns={"http://www.w3.org/2000/svg"}
+      fill={"none"}
+      viewBox={"0 0 24 24"}
+      height={"1em"}
+      width={"1em"}
+      style={{
+        stroke: "currentcolor",
+        ...(style || {}),
+      }}
+      className={classNames("plasmic-default__svg", className)}
+      {...restProps}
+    >
+      {title && <title>{title}</title>}
+
+      <path
+        d={"M13 10V3L4 14h7v7l9-11h-7z"}
+        stroke={"currentColor"}
+        strokeWidth={"2"}
+        strokeLinecap={"round"}
+        strokeLinejoin={"round"}
+      ></path>
+    </svg>
+  );
+}
+
+export default LightningBoltIcon;
+/* prettier-ignore-end */
---
+++ src/components/plasmic/mcusa/icons/PlasmicIcon__Minus.jsx
@@ -0,0 +1,37 @@
+// @ts-nocheck
+/* eslint-disable */
+/* tslint:disable */
+/* prettier-ignore-start */
+import React from "react";
+import { classNames } from "@plasmicapp/react-web";
+
+export function MinusIcon(props) {
+  const { className, style, title, ...restProps } = props;
+  return (
+    <svg
+      xmlns={"http://www.w3.org/2000/svg"}
+      fill={"none"}
+      viewBox={"0 0 20 20"}
+      height={"1em"}
+      width={"1em"}
+      style={{
+        fill: "currentcolor",
+        ...(style || {}),
+      }}
+      className={classNames("plasmic-default__svg", className)}
+      {...restProps}
+    >
+      {title && <title>{title}</title>}
+
+      <path
+        fillRule={"evenodd"}
+        clipRule={"evenodd"}
+        d={"M3 10a1 1 0 011-1h12a1 1 0 010 2H4a1 1 0 01-1-1z"}
+        fill={"currentColor"}
+      ></path>
+    </svg>
+  );
+}
+
+export default MinusIcon;
+/* prettier-ignore-end */
