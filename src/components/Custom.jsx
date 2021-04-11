e09c708aafc461602b02e7331ad45efd0b8f84be
Rose Scott <rose@mobicycle.eu>
2021-04-11 17:12:42 +0100

    content updates

--- src/components/Custom.jsx
+++ src/components/Custom.jsx
@@ -161,7 +161,7 @@ const Custom = () => {
         <div className="w-full lg:w-1/3 px-4 mb-6 lg:mb-0">
           <div className="p-12 bg-gray-50 rounded lg:text-center">
             <h3 className="text-2xl font-bold text-gray-500">Starter</h3>
-            <span className="inline-block mb-6 text-6xl font-bold font-heading">$34.99</span>
+            <span className="inline-block mb-6 text-5xl font-bold font-heading">$34.99</span>
             <ul className="mb-6 text-left text-lg">
               <li className="mb-2 flex items-center">
                 <svg className="mr-2 w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -194,7 +194,7 @@ const Custom = () => {
         <div className="w-full lg:w-1/3 px-4 mb-6 lg:mb-0">
           <div className="p-12 bg-gray-500 rounded lg:text-center">
             <h3 className="text-2xl font-bold text-gray-50">Pro</h3>
-            <span className="inline-block mb-6 text-6xl font-bold text-white">$65.99</span>
+            <span className="inline-block mb-6 text-5xl font-bold text-white">$65.99</span>
             <ul className="mb-6 text-left text-lg">
               <li className="mb-2 flex items-center">
                 <svg className="mr-2 w-6 h-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -233,7 +233,7 @@ const Custom = () => {
         <div className="w-full lg:w-1/3 px-4">
           <div className="p-12 bg-gray-50 rounded lg:text-center">
             <h3 className="text-2xl font-bold text-gray-500">Premium</h3>
-            <span className="inline-block mb-6 text-6xl font-bold font-heading">$99.99</span>
+            <span className="inline-block mb-6 text-5xl font-bold font-heading">$99.99</span>
             <ul className="mb-6 text-left text-lg">
               <li className="mb-2 flex items-center">
                 <svg className="mr-2 w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
